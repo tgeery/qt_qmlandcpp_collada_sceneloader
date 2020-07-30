3e4f966f9abda05eacd801deaf232a2ccb97aee6
tgeery <tpgeery@socal.rr.com>
2020-07-29 22:36:09 -0700

    display qgraphicsscene and qt3dwindow

--- dae_import.pro
+++ dae_import.pro
@@ -16,11 +16,9 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    main.cpp \
-    mainwindow.cpp
+    main.cpp
 
-HEADERS += \
-    mainwindow.h
+HEADERS +=
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include "mainwindow.h"
-
 #include <QApplication>
 #include <Qt3DCore/QEntity>
 #include <Qt3DWindow>
@@ -7,55 +5,66 @@
 #include <Qt3DExtras/Qt3DExtras>
 #include <Qt3DQuickExtras/Qt3DQuickExtras>
 #include <QGraphicsView>
+#include <QGraphicsProxyWidget>
 #include <QGuiApplication>
 #include <QUrl>
 #include <QVector>
+#include <QVBoxLayout>
+
+class Scene : public Qt3DExtras::Qt3DWindow
+{
+public:
+    Scene(QWidget * parent = nullptr) : Qt3DExtras::Qt3DWindow()
+    {
+    //    this->defaultFrameGraph()->setClearColor(Qt::gray);
+        Qt3DCore::QEntity * sceneRoot = new Qt3DCore::QEntity;
+
+        // Scene Camera
+        Qt3DRender::QCamera *camera = this->camera();
+        camera->setProjectionType(Qt3DRender::QCameraLens::PerspectiveProjection);
+        camera->setViewCenter(QVector3D(0.0f, 0.0f, 0.0f));
+        camera->setPosition(QVector3D(0.0f, 0.0f, 5.0f));
+        camera->setNearPlane(0.001f);
+        camera->setFarPlane(10000.0f);
+
+        // For camera controls
+        Qt3DExtras::QFirstPersonCameraController *camController = new Qt3DExtras::QFirstPersonCameraController(sceneRoot);
+        camController->setCamera(camera);
+
+        Qt3DRender::QSceneLoader * sceneLoader = new Qt3DRender::QSceneLoader(sceneRoot);
+        sceneLoader->setObjectName("monkey_face.dae");
+        sceneRoot->addComponent(sceneLoader);
+        sceneLoader->setSource(QUrl::fromLocalFile("C:\\Users\\tpgee\\Desktop\\monkey_face.dae"));
+        this->setRootEntity(sceneRoot);
+    }
+
+    ~Scene() {
+    }
+
+private:
+};
 
 int main(int argc, char *argv[])
 {
-//    QSurfaceFormat format = QSurfaceFormat::defaultFormat();
-//    format.setRenderableType(QSurfaceFormat::OpenGLES);
-//    QSurfaceFormat::setDefaultFormat(format);
-
     QApplication a(argc, argv);
-    MainWindow w;
 
 //    Qt3DExtras::Quick::Qt3DQuickWindow view;
 //    view.engine()->qmlEngine()->rootContext()->setContextProperty("_window", &view);
 //    view.setSource(QUrl("../dae_import/main.qml"));
 //    view.show();
 
-    Qt3DExtras::Qt3DWindow view;
-//    view.defaultFrameGraph()->setClearColor(Qt::gray);
-    Qt3DCore::QEntity * sceneRoot = new Qt3DCore::QEntity;
-
-    // Scene Camera
-    Qt3DRender::QCamera *camera = view.camera();
-    camera->setProjectionType(Qt3DRender::QCameraLens::PerspectiveProjection);
-    camera->setViewCenter(QVector3D(0.0f, 3.5f, 0.0f));
-    camera->setPosition(QVector3D(0.0f, 3.5f, 25.0f));
-    camera->setNearPlane(0.001f);
-    camera->setFarPlane(10000.0f);
-
-    // For camera controls
-    Qt3DExtras::QFirstPersonCameraController *camController = new Qt3DExtras::QFirstPersonCameraController(sceneRoot);
-    camController->setCamera(camera);
-
-    Qt3DCore::QEntity * sceneLoaderEntity = new Qt3DCore::QEntity(sceneRoot);
-    Qt3DRender::QSceneLoader * sceneLoader = new Qt3DRender::QSceneLoader(sceneLoaderEntity);
-    sceneLoader->setObjectName("monkey_face.dae");
-    sceneLoaderEntity->addComponent(sceneLoader);
-    sceneLoader->setSource(QUrl::fromLocalFile("C:\\Users\\tpgee\\Desktop\\monkey_face.dae"));
-    view.setRootEntity(sceneRoot);
-//    view.show();
-
-    QGraphicsView * gview = new QGraphicsView;
+    QWidget * ab = new QWidget;
+    QVBoxLayout * lyt = new QVBoxLayout;
     QGraphicsScene * scene = new QGraphicsScene;
+    Scene * sc = new Scene;
     scene->setBackgroundBrush(Qt::green);
-    gview->setScene(scene);
-//    gview->show();
-    view.show();
+    QWidget * w = QWidget::createWindowContainer(sc);
+    w->setFixedSize(200,200);
+    QGraphicsView * view = new QGraphicsView(scene);
+    lyt->addWidget(view);
+    lyt->addWidget(w);
+    ab->setLayout(lyt);
+    ab->show();
 
-//    w.show();
     return a.exec();
 }
